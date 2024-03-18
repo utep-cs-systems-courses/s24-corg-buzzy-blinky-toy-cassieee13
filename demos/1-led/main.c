d18277016ba825ddab425ea48c34344c5937f911
Cassandra Alvarez <cealvarez2@miners.utep.edu>
2024-03-18 14:19:19 -0600

    Worked on demo 1,2,3. Made progress learning how to blink the LEDS and dim

--- demos/1-led/main.c
+++ demos/1-led/main.c
@@ -5,8 +5,7 @@
 
 int main(void) {
   P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
-
+  P1OUT &= ~LED_GREEN; //switching to |= LED_GREEN turns it on
+  P1OUT |= LED_RED; //switching to &= ~LED_RED makes it turn off
   or_sr(0x18);		/* CPU off, GIE on */
 }
